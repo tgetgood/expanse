004e6b51933ac47b97063ac65bf1c4620d589d32
Thomas Getgood <thomas.getgood@gmail.com>
2017-12-04 12:05:16 -0500

    Proper config.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/clojurescript "1.9.946"]]
 
   :plugins [[lein-figwheel "0.5.14"]
-            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+            [lein-cljsbuild "1.1.7" :exclusions [org.clojure/clojure]]]
 
   :source-paths ["src"]
 
@@ -19,7 +19,7 @@
    [{:id           "dev"
      :source-paths ["src"]
 
-     :figwheel     {:on-jsload "expanse.core/on-js-reload"}
+     :figwheel     {:on-jsload "expanse.core/on-reload"}
 
      :compiler     {:main                 expanse.core
                     :asset-path           "js/compiled/out"
@@ -29,6 +29,7 @@
                     :checked-arrays       :warn
                     :source-map-timestamp true
                     :preloads             [devtools.preload]}}
+
     {:id           "min"
      :source-paths ["src"]
      :compiler     {:output-to      "resources/public/js/compiled/expanse.js"
@@ -39,7 +40,7 @@
                     :pretty-print   false}}]}
 
   :profiles
-  {:prod {:dependencies [macroexpanse/lemonade "0.1.0"]}
+  {:prod {:dependencies [[macroexpanse/lemonade "0.1.0"]]}
    :dev  {:dependencies  [[binaryage/devtools "0.9.4"]
                           [org.clojure/spec.alpha "0.1.134"]
                           [org.clojure/tools.namespace "0.2.11"]
@@ -48,8 +49,9 @@
                            :exclusions [org.clojure/core.async]]
                           [com.cemerick/piggieback "0.2.2"]
                           [org.clojure/test.check "0.9.0"]]
-          ;; Convenience for developing two pacakages in tandem
+          ;; need to add dev source path here to get user.clj loaded
           :source-paths  ["src" "dev" "../lemonade/src"]
+
           :repl-options  {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
           ;; need to add the compliled assets to the :clean-targets
           :clean-targets ^{:protect false} ["resources/public/js/compiled"
